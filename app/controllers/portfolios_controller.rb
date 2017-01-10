82fb09d55bf52a6084384b5026858ed4487c1e0c
jamisongrant <jgbatchelor@gmail.com>
2017-01-10 14:24:20 -0700

    added routes, stylesheet, link tos, and form file with index.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,23 +1,55 @@
 class PortfoliosController < ApplicationController
+  
   def index
     @portfolio_items = Portfolio.all
   end
+
   def new
     @portfolio_item = Portfolio.new
   end 
 
-
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
-  respond_to do |format|
+    @portfolio_item = Portfolio.new(portfolio_params)
+    
+    respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'YOUR portfolio item SUCKS.' }
-      
       else
         format.html { render :new }
-
       end
     end
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    if @portfolio_item.update(portfolio_params)
+      redirect_to @portfolio_item  
+    else  
+      render :edit
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])  
+    @portfolio_item.destroy
+    redirect_to portfolios_path
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image, :main_image)
+  end
+
+
+
 end
