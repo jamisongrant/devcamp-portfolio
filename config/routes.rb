f98b5b741ce8829f6cb319fd5c004a925f644f0f
jamisongrant <jgbatchelor@gmail.com>
2017-01-16 14:09:40 -0700

    added placeholder concern

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios
 
+  root to: 'pages#home'
+
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
@@ -8,8 +10,10 @@ Rails.application.routes.draw do
   resources :blogs do
     member do
     get :toggle_status
-         end
+      end
     end
-
-  root to: 'pages#home'
+  resources :portfolios, except: [:show]
+  get 'portfolio/id', to: 'portfolios#show', as: 'show_portfolio'
+  get 'angular-items', to: 'portfolios#angular'
 end
+
