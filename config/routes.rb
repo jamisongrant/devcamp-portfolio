64d198a9d5adbc85823ee6631a5d89de9ac8ce29
jamisongrant <jgbatchelor@gmail.com>
2017-01-18 15:38:40 -0700

    add espresso and style

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
+  get 'portfolio/id', to: 'portfolios#show', as: 'show_portfolio'
+  get 'angular-items', to: 'portfolios#angular'
 
   resources :blogs do
     member do
@@ -15,7 +17,6 @@ Rails.application.routes.draw do
       end
     end
   resources :portfolios, except: [:show]
-  get 'portfolio/id', to: 'portfolios#show', as: 'show_portfolio'
-  get 'angular-items', to: 'portfolios#angular'
+ 
 end
 
