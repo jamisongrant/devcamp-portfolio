363d333367517182322f647cfe64bfd03d2be6e4
jamisongrant <jgbatchelor@gmail.com>
2017-01-30 17:07:31 -0700

    implement initial file uploading sytem via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,14 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies,
                                 allow_destroy: true,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
-    validates_presence_of :title, :body, :main_image, :thumb_image
+
+  include Placeholder
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
     def self.angular
       where(subtitle: 'Angular')
