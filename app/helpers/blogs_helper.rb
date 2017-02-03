5ebfd2c3bee28394bee8a4036ff264963b15ea78
jamisongrant <jgbatchelor@gmail.com>
2017-02-03 16:56:44 -0700

    add default retro comment feature

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,6 @@
 module BlogsHelper
   def gravatar_helper user
-    image_tag "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
+    image_tag "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?d=retro", width: 40
   end
 
   class CodeRayify < Redcarpet::Render::HTML
