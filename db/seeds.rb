df1718d0bb8376a4fe4f44f5edcc102af452267e
jamisongrant <jgbatchelor@gmail.com>
2017-02-06 11:48:56 -0700

    add validations for topics in blogs and added a topic form element

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# 3.times do |topic|
-#   Topic.create!(
-#     title: "Topic #{topic}"
-# )
-# end
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
 
-# puts "3 Topics created"
+puts "3 Topics created"
 
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body:  "Nam nisl mauris, tristique sed enim et, porta ullamcorper ligula. Nulla pulvinar lectus in auctor auctor. Maecenas accumsan, nisl at luctus feugiat, augue ante porttitor ante, eget cursus purus libero at enim. Sed in interdum felis. Duis at fringilla velit. Duis sit amet diam eget lorem tempus sagittis et pretium orci. Mauris vitae blandit nunc. Vivamus cursus mollis ex eget sollicitudin. Nulla facilisi. Aliquam varius risus in orci malesuada tempus. Nulla condimentum enim sit amet varius porttitor. Aenean ut risus maximus, bibendum dolor id, lacinia augue. Sed vulputate vulputate erat quis elementum. Proin vitae ante quis tellus commodo ultricies. Quisque rutrum, eros sed malesuada posuere, massa felis efficitur arcu, sed posuere dui urna sed turpis. "
-    # topic_id: Topic.last.id
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    topic_id: Topic.last.id
   )
 end
 
@@ -19,18 +19,17 @@ puts "10 blog posts created"
 5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
-  percent_utilized: 15
+    percent_utilized: 15
   )
 end
 
-
 puts "5 skills created"
 
-9.times do |portfolio_item|
+1.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
-    body: "Section 1.10.33 of \"de Finibus Bonorum et Malorum\", written by Cicero in 45 BC",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
   )
@@ -38,3 +37,10 @@ end
 
 puts "9 portfolio items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
